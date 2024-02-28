07fd819d5c00488da456ef7dc89eb608a9e28a81
Flor <flopypillas1978@gmail.com>
2024-02-27 23:34:39 -0300

    Configuración render

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
 const path = require ("path");
+
+const PORT = process.env.PORT || 3000
+
 // ruta absoluta carpeta public
 const publicPath = path.resolve(__dirname , "./public");
 app.use (express.static (publicPath));
-app.listen(3030, ()=>
-    console.log("servidor corriendo en puerto 3030")
+app.listen(PORT, ()=>
+    console.log(`servidor corriendo en puerto ${PORT}`)
 )
 
 app.get("/", (req,res)=>{
